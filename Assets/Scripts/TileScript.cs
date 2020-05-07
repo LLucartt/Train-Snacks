03b0140624c464de11d6d4e36818ca41b84613bb
LLucartt <60555543+LLucartt@users.noreply.github.com>
2020-05-07 15:22:20 +1200

    Initial commit

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    [Header("Tiles")]
+    public Sprite[] tileGraphics;
+    public float hoverAmount;
+    public LayerMask obstacleLayer;
+
+    [Header("Highlight")]
+    public Color highlightedColor;
+    public bool isWalkable;
+    public bool isClearBool = true;
+
+    GameMaster gm;
+
+    private void Start(){
+      sr = GetComponent<SpriteRenderer>();
+      int randTile = Random.Range(0, tileGraphics.Length);
+      sr.sprite = tileGraphics[randTile];
+
+      gm = GameMaster.current;
+    }
+
+    void Update(){/*
+      Collider2D obstacle = Physics2D.OverlapCircle(transform.position, 0.2f, obstacleLayer);
+      if(obstacle != null){
+          isClearBool = false;
+      } else{
+          isClearBool = true;
+      }*/
+    }
+
+    private void OnMouseEnter(){
+      IncreaseTileSize();
+    }
+
+    private void OnMouseExit(){
+      ReturnTileSize();
+    }
+
+    private void IncreaseTileSize(){
+      transform.localScale += Vector3.one * hoverAmount;
+    }
+
+    private void ReturnTileSize(){
+      transform.localScale -= Vector3.one * hoverAmount;
+    }
+
+    public bool isClear(){
+      Collider2D obstacle = Physics2D.OverlapCircle(transform.position, 0.2f, obstacleLayer); //location, radius, recognise what objects
+      if(obstacle != null){
+          return false;
+          isClearBool = false;
+      } else{
+          return true;
+          isClearBool = true;
+      }
+    }
+
+    public void Highlight(){
+      sr.color = highlightedColor;
+      isWalkable = true;
+    }
+    public void Reset(){
+      sr.color = Color.white;
+      isWalkable = false;
+    }
+
+    private void OnMouseDown(){
+      if(isWalkable && gm.selectedUnit != null){
+        gm.selectedUnit.Move(this.transform.position);
+      }
+    }
+
+}
---
+++ Assets/Scripts/UnitScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    public bool selected;
+    GameMaster gm;
+    public int tileSpeed;
+    public bool hasMoved;
+    public float moveSpeed;
+
+    private void Start(){
+      gm = GameMaster.current;
+    }
+
+    private void OnMouseDown(){
+      if(selected == true){
+          selected = false;
+          gm.selectedUnit = null;
+          gm.ResetTiles();
+      } else{
+          if(gm.selectedUnit != null){
+            gm.selectedUnit.selected = false;
+          }
+
+          selected = true;
+          gm.selectedUnit = this;
+
+          gm.ResetTiles();
+          GetWalkableTiles();
+      }
+    }
+
+    void GetWalkableTiles(){
+      if(hasMoved == true){
+        return;
+      }
+
+      foreach(GameObject tile in gm.tiles){
+        if(Mathf.Abs(transform.position.x - tile.transform.position.x) + Mathf.Abs(transform.position.y - tile.transform.position.y) <= tileSpeed){
+          if(tile.GetComponent<TileScript>().isClear() == true){
+            tile.GetComponent<TileScript>().Highlight();
+          }
+        }
+      }
+    }
+
+    public void Move(Vector2 tilePos){
+      gm.ResetTiles();
+      StartCoroutine(StartMovement(tilePos));
+    }
+
+    IEnumerator StartMovement(Vector2 tilePos){
+      while(transform.position.x != tilePos.x){
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(tilePos.x, transform.position.y), moveSpeed * Time.deltaTime);
+        yield return null;
+      }
+      while(transform.position.y != tilePos.y){
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, tilePos.y), moveSpeed * Time.deltaTime);
+        yield return null;
+      }
+
+      hasMoved = true;
+    }
+
+}
